f7951738828595ab59c2d354a88148f4cee06f5c
Rythm <rythmrobin@gmail.com>
2025-06-02 00:20:32 +0530

    changed icon

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -21,7 +21,7 @@ export const generateMetadata = ({
     icons = [
         {
             rel: "icon",
-            url: "/icons/icon-dark.png",
+            url: "/icons/icon.png",
             media: "(prefers-color-scheme: light)",
         },
         {
