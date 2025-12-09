6cbd0c8becd97bfe69a8c8b2fc9d08de5dfc6b49
Ray <ray@rhythmiqcx.com>
2025-12-09 14:23:28 +0530

    space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
                     heading.variable,
                     subheading.variable,
                 )}
-            >                        
+            >                
+                    
                     <Toaster richColors theme="dark" position="top-right" />
                     {children}
                     <Analytics />
